5391e204b4027bf124dde1d1628885d630995aca
Aditya_Mishra <adityasaiyansuper@gmail.com>
2024-07-22 15:34:09 +0530

    update format of user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,18 @@
 import { Router } from "express";
-import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
-import {upload} from '../middlewares/multer.middleware.js'
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -10,42 +22,45 @@ router.route("/register").post(
   upload.fields([
     {
       name: "avatar",
-      maxCount: 1
+      maxCount: 1,
     },
     {
       name: "coverImage",
-      maxCount: 1
-    }
+      maxCount: 1,
+    },
   ]),
   registerUser
-  );
+);
 
-  // user login
-  router.route("/login").post(loginUser);
+// user login
+router.route("/login").post(loginUser);
 
+/***** Secured Routes *****/
+// user logout
+router.route("/logout").post(verifyJWT, logoutUser);
+// next() from verifyJWT (middleware) helps routers to know there is another function and execute it
 
-  /***** Secured Routes *****/
-  // user logout 
-  router.route("/logout").post(verifyJWT, logoutUser)
-  // next() from verifyJWT (middleware) helps routers to know there is another function and execute it
+// verifJWT -> to check user should be logged
 
-  // verifJWT -> to check user should be logged in
+// refresh token endpoint
+router.route("/refresh-token").post(refreshAccessToken);
 
-  // refresh token endpoint
-  router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 
-  router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser);
 
-  router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 
-  router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 
-  router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
-  router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
 
-  router.route("/channel/:username").get(verifyJWT, getUserChannelProfile)
-
-  router.route("/watch-history").get(verifyJWT, getWatchHistory)
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 export default router;
